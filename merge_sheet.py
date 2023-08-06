cfa29206420990b3312f57f96ecddf48c17210bc
VincentLi1216 <sunnus.tw@gmail.com>
2023-08-06 09:51:21 +0800

    finish merge_sheet.py

--- merge_sheet.py
+++ merge_sheet.py
@@ -1,12 +1,19 @@
 import pandas as pd
+import os
 
 def merge_csv_files(a_csv_path, b_csv_path, output_csv_path, pk):
+    
+    for path in [a_csv_path, b_csv_path]:
+        if not os.path.exists(path):
+            print(f"!!!!\"{path}\" doesn't exists!!!!")
+            return
+
     # 讀取A版和B版的CSV檔
     a_df = pd.read_csv(a_csv_path)
     b_df = pd.read_csv(b_csv_path)
 
-    # 合併兩個DataFrame，只保留B版中有更新的內容
-    merged_df = a_df.merge(b_df, on=pk, how='left', suffixes=('', '_y'))
+    # 合併兩個DataFrame，保留B版中所有的內容，並補上A版可能缺失的資料
+    merged_df = pd.merge(a_df, b_df, on=pk, how='outer', suffixes=('', '_y'))
 
     # 更新A版的資料
     for column in a_df.columns:
@@ -15,6 +22,12 @@ def merge_csv_files(a_csv_path, b_csv_path, output_csv_path, pk):
 
     # 寫入新的CSV檔
     merged_df.to_csv(output_csv_path, index=False)
+    print(f"合併已完成")
+    print(f"合併檔在在\"{output_path}\"")
 
-# 呼叫函式
-merge_csv_files('活頁簿A.csv', '活頁簿B.csv', '輸出.csv', 'pk')
+if __name__ == "__main__":
+    a_path = input("請輸入你的.csv檔案位置：")
+    b_path = input("請輸入新的.csv檔案位置：")
+    output_path = input("請輸入新的檔案名稱：")
+    merge_csv_files(a_path, b_path, output_path, "資產編號")
+
